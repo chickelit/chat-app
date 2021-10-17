0a8c281eb1c4c5572287b9fb966899f1517efb7e
José Anselmo <anselmojose297@gmail.com>
2021-10-17 13:58:43 -0300

    Add some modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,24 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'frontend',
+    title: "frontend",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-    ]
+    link: [],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "normalize.css/normalize.css"
-  ],
+  css: ["normalize.css/normalize.css", "@assets/scss/base.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [{ path: "@/components/", pathPrefix: false }],
@@ -30,14 +26,16 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: ["@nuxtjs/style-resources"],
+
+  styleResources: {
+    scss: ["@/components/bosons/*.scss"],
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
