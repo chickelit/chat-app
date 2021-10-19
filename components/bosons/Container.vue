e2513c991d27f48de9bcbbb8d93d4e324a8d7f04
José Anselmo <anselmojose297@gmail.com>
2021-10-19 19:37:13 -0300

    Update container boson

--- components/bosons/Container.vue
+++ components/bosons/Container.vue
@@ -5,7 +5,17 @@
 </template>
 
 <style lang="scss" scoped>
-.container {
-  width: 80%;
-}
+  .container {
+    margin: 0 auto;
+    max-width: 1070px;
+    @include screen("small") {
+      width: 90%;
+    }
+    @include screen("medium") {
+      width: 85%;
+    }
+    @include screen("large", "infinity") {
+      width: 70%;
+    }
+  }
 </style>
