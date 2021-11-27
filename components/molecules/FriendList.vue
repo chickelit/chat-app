fdba6d2bfcbffdb0be69acc41b62218c60ef9501
jose13162 <anselmojose297@gmail.com>
2021-11-27 13:41:07 -0300

    Adjust friend components

---
+++ components/molecules/FriendList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="friends-list">
+    <div v-for="(friend, index) of friends" :key="index">
+      <FriendCard :index="index" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      friends: Array(15).fill(false),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.friends-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  grid-auto-flow: row;
+  div {
+    &:nth-last-child(1) {
+      ::v-deep.friend-card {
+        .friend-card-dropdown {
+          top: -1rem;
+        }
+      }
+    }
+  }
+}
+</style>
--- components/organisms/Friends.vue
+++ components/organisms/Friends.vue
@@ -1,80 +1,24 @@
 <template>
   <div class="friends">
     <Wrapper>
-      <div class="container">
-        <ul class="friends-list">
-          <li v-for="(friend, index) of friends" :key="index" class="friend">
-            <FriendCard :index="index" />
-          </li>
-        </ul>
-      </div>
+      <FriendList class="friend-list" />
     </Wrapper>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-export default Vue.extend({
-  data() {
-    return {
-      friends: Array(15).fill(false),
-    };
-  },
-  methods: {
-    setView({ target }: Event) {
-      const element = target as Element;
-      const navigationItems = document.querySelectorAll(
-        ".friends-navigation-item"
-      )!;
-
-      navigationItems.forEach((item) => {
-        if (item.classList.contains("active")) {
-          item.classList.remove("active");
-        }
-      });
-
-      if (!element.classList.contains("active")) {
-        element.classList.add("active");
-      }
-    },
-  },
-});
-</script>
 
 <style lang="scss" scoped>
 ::-webkit-scrollbar {
   width: 0px;
 }
 .friends {
+  position: relative;
+  width: 100%;
   height: 100%;
-  background: color("dark", "darker");
-  position: absolute;
-  inset: 0;
-  overflow-y: scroll;
-  display: grid;
-  grid-template-rows: 1fr;
-  .container {
+  background: color("dark");
+  .friend-list {
     position: absolute;
     inset: 0;
-    .friends-list {
-      position: absolute;
-      inset: 0;
-      overflow-y: scroll;
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-auto-rows: max-content;
-      grid-auto-flow: row;
-      gap: 0.125rem;
-      .friend {
-        &:nth-last-child(1) {
-          ::v-deep.friend-card {
-            .friend-card-dropdown {
-              top: -1rem;
-            }
-          }
-        }
-      }
-    }
+    overflow-y: auto;
   }
 }
 </style>
