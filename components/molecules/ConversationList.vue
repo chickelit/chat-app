fe5b3bf208511d5c437ddbb60354dd14f1a95aef
jose13162 <anselmojose297@gmail.com>
2021-11-27 13:15:55 -0300

    Adjusting conversation components

---
+++ components/molecules/ConversationList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="conversations-list">
+    <div
+      v-for="(conversation, index) of conversations"
+      :key="index"
+      class="conversation"
+    >
+      <ConversationCard
+        aria-label="Conversar com <username>"
+        :index="index"
+        @click="
+          setView({
+            newView: 'ConversationChat',
+            previousView: $view,
+          })
+        "
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { setView } from "@/utils";
+import { view } from "~/store";
+export default Vue.extend({
+  data() {
+    return {
+      conversations: Array(15).fill(false),
+      setView,
+    };
+  },
+  computed: {
+    $view() {
+      return view.$view;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.conversations-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  grid-auto-flow: row;
+  gap: 0.125rem;
+}
+</style>
--- components/organisms/Conversations.vue
+++ components/organisms/Conversations.vue
@@ -1,64 +1,24 @@
 <template>
   <div class="conversations">
     <Wrapper>
-      <ul class="conversations-list">
-        <li
-          v-for="(conversation, index) of conversations"
-          :key="index"
-          class="conversation"
-        >
-          <ConversationCard
-            aria-label="Conversar com <username>"
-            :index="index"
-            @click="
-              setView({
-                newView: 'ConversationChat',
-                previousView: $view,
-              })
-            "
-          />
-        </li>
-      </ul>
+      <ConversationList class="conversation-list" />
     </Wrapper>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from "vue";
-import { setView } from "@/utils";
-import { view } from "~/store";
-export default Vue.extend({
-  data() {
-    return {
-      conversations: Array(15).fill(false),
-      setView,
-    };
-  },
-  computed: {
-    $view() {
-      return view.$view;
-    },
-  },
-});
-</script>
 
 <style lang="scss" scoped>
 ::-webkit-scrollbar {
   width: 0px;
 }
 .conversations {
-  position: absolute;
-  inset: 0;
+  position: relative;
+  width: 100%;
+  height: 100%;
   background: color("dark", "darker");
   .conversations-list {
     position: absolute;
     inset: 0;
-    overflow-y: scroll;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-auto-rows: max-content;
-    grid-auto-flow: row;
-    gap: 0.125rem;
+    overflow-y: auto;
   }
 }
 </style>
