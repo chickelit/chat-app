921d34d18d506c1a7b74962f0e52e80ec5a9ffca
jose13162 <anselmojose297@gmail.com>
2021-11-27 13:41:21 -0300

    Adjust friendship request components

---
+++ components/molecules/FriendshipRequestList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="friendship-requests-list">
+    <div v-for="(friendshipRequest, index) of friendshipRequests" :key="index">
+      <FriendshipRequestCard :index="index" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      friendshipRequests: Array(15).fill(false),
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.friendship-requests-list {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: max-content;
+  grid-auto-flow: row;
+  div {
+    &:nth-last-child(1) {
+      ::v-deep.friendship-request-card {
+        .friendship-request-card-dropdown {
+          top: -1.5rem;
+        }
+      }
+    }
+  }
+}
+</style>
--- components/organisms/FriendshipRequests.vue
+++ components/organisms/FriendshipRequests.vue
@@ -1,17 +1,7 @@
 <template>
   <div class="friendship-requests">
     <Wrapper>
-      <div class="container">
-        <ul class="friendship-requests-list">
-          <li
-            v-for="(friend, index) of friends"
-            :key="index"
-            class="friendship-request"
-          >
-            <FriendshipRequestCard :index="index" />
-          </li>
-        </ul>
-      </div>
+      <FriendshipRequestList />
     </Wrapper>
   </div>
 </template>
@@ -23,24 +13,6 @@ export default Vue.extend({
     return {
       friends: Array(15).fill(false),
     };
-  },
-  methods: {
-    setView({ target }: Event) {
-      const element = target as Element;
-      const navigationItems = document.querySelectorAll(
-        ".friendship-requests-navigation-item"
-      )!;
-
-      navigationItems.forEach((item) => {
-        if (item.classList.contains("active")) {
-          item.classList.remove("active");
-        }
-      });
-
-      if (!element.classList.contains("active")) {
-        element.classList.add("active");
-      }
-    },
   },
 });
 </script>
@@ -57,28 +29,10 @@ export default Vue.extend({
   overflow-y: scroll;
   display: grid;
   grid-template-rows: 1fr;
-  .container {
+  .friendship-requests-list {
     position: absolute;
     inset: 0;
-    .friendship-requests-list {
-      position: absolute;
-      inset: 0;
-      overflow-y: scroll;
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-auto-rows: max-content;
-      grid-auto-flow: row;
-      gap: 0.125rem;
-      .friendship-request {
-        &:nth-last-child(1) {
-          ::v-deep.friendship-request-card {
-            .friendship-request-card-dropdown {
-              top: -1.5rem;
-            }
-          }
-        }
-      }
-    }
+    overflow-y: scroll;
   }
 }
 </style>
