5a2150e267122516c93b1dd00a4e35385e32ce33
jose13162 <anselmojose297@gmail.com>
2021-12-08 10:02:20 -0300

    Use friends store

--- components/molecules/FriendList.vue
+++ components/molecules/FriendList.vue
@@ -1,31 +1,100 @@
 <template>
-  <div class="friends-list">
-    <div v-for="(friend, index) of friends" :key="index">
-      <FriendCard :index="index" />
+  <div class="friend-list" @scroll="checkScroll">
+    <div
+      v-for="(friend, index) of friends"
+      :key="index"
+      :class="{ last: index === friends.length - 1 }"
+    >
+      <FriendCard v-if="!friend" :index="index" />
+      <FriendCard v-else :index="index" :friend="friend" />
+    </div>
+    <div v-show="friends.length < $meta.total" class="loading-wrapper">
+      <Loading :active="loading" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { friend } from "~/store";
 export default Vue.extend({
   data() {
     return {
-      friends: Array(15).fill(false),
+      friends: Array(20).fill(false),
+      page: 1,
+      loading: false,
     };
+  },
+  computed: {
+    $meta() {
+      return friend.$meta;
+    },
+  },
+  async mounted() {
+    if (friend.$all.length > 0) {
+      const friends = friend.$all;
+
+      this.friends = friends;
+    } else {
+      try {
+        await friend.index({ page: this.page, perPage: 20 });
+
+        const friends = friend.$all;
+
+        this.friends = friends;
+      } catch (e) {
+        this.$notify({
+          type: "error",
+          title: "Ops...",
+          text: "Houve um erro ao listar seus amigos...",
+        });
+      }
+    }
+  },
+  methods: {
+    async checkScroll(event: any) {
+      const target = event.target as HTMLElement;
+
+      if (
+        target.scrollHeight - 200 <= target.scrollTop + target.clientHeight &&
+        !this.loading
+      ) {
+        if (this.page < friend.$meta.last_page) {
+          this.page += 1;
+          this.loading = true;
+
+          await friend.index({ page: this.page, perPage: 20 });
+
+          const friends = friend.$all;
+
+          this.friends = friends;
+          this.loading = false;
+        }
+      } else {
+        this.$emit("notFullScrolled");
+      }
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.friends-list {
+.loading-wrapper {
+  height: 5rem;
+  width: 5rem;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  justify-self: center;
+}
+.friend-list {
   display: grid;
   grid-template-columns: 1fr;
   grid-auto-rows: max-content;
   grid-auto-flow: row;
-  div {
-    &:nth-last-child(1) {
-      ::v-deep.friend-card {
+  .last {
+    ::v-deep.friend-card {
+      .wrapper {
         .friend-card-dropdown {
           top: -1rem;
         }
