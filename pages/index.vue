961e5879834f8d916fdf8ba28d84f75a1c8ecab4
jose13162 <anselmojose297@gmail.com>
2022-01-04 17:41:04 -0300

    Add auth middleware for some pages and start socket integration

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,24 @@
 <script lang="ts">
 import Vue from "vue";
 import auth from "@/middleware/auth";
+import socket from "~/plugins/socket.client";
+import { friendship, friendshipRequest, profile } from "~/store";
 export default Vue.extend({
   middleware: auth,
+  created() {
+    socket.emit("create", `user-${profile.$single.id}`);
+
+    socket.on("newFriendshipRequest", (data) => {
+      friendshipRequest.updateFriendshipRequests([data]);
+    });
+
+    socket.on("friendshipRequestAccepted", (data) => {
+      console.log(data);
+    });
+
+    socket.on("newFriend", (user) => {
+      friendship.updateFriends([user]);
+    });
+  },
 });
 </script>
